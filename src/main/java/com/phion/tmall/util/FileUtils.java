6e7c5a8f1f4230f42fa8fac3325d1cfd920a4e4f
martin-YFL <15037887317@163.com>
2019-11-30 13:51:28 +0800

    添加图片处理工具类，文件处理工具类

---
+++ src/main/java/com/phion/tmall/util/FileUtils.java
@@ -0,0 +1,52 @@
+package com.phion.tmall.util;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
+import com.phion.tmall.config.ResourceConfigAdapter;
+
+public class FileUtils {
+	
+	public static String getResourcesPath() {
+		String path = System.getProperty("user.dir")+"\\src\\main\\resources\\";
+		return path;
+	}
+	
+	/**
+	 * 获取项目名
+	 * @return
+	 */
+	public static String getWorkareaPath() {
+		URL url = ResourceConfigAdapter.class.getClassLoader().getResource("application.properties");
+		File file = new File(url.getFile());
+		String path = getProperties(file.getAbsolutePath(),"server.context-path").substring(1);
+		System.out.println(path);
+		return path;
+	}
+
+	/**
+	 * 读取配置文件
+	 * @param filePath
+	 * @param keyWord
+	 * @return
+	 */
+	public static String getProperties(String filePath, String keyWord) {
+		Properties prop = new Properties();
+		String value = null;
+		try {
+			// 通过输入缓冲流进行读取配置文件
+			InputStream InputStream = new BufferedInputStream(new FileInputStream(new File(filePath)));
+			// 加载输入流
+			prop.load(InputStream);
+			// 根据关键字获取value值
+			value = prop.getProperty(keyWord);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return value;
+	}
+}
