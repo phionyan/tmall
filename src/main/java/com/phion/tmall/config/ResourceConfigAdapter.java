1361826402e1074a74df108b24095caea8ae7308
martin-YFL <15037887317@163.com>
2019-11-30 13:53:20 +0800

    添加资源文件映射，使得上传到static文件夹下的文件可以直接访问

---
+++ src/main/java/com/phion/tmall/config/ResourceConfigAdapter.java
@@ -0,0 +1,35 @@
+package com.phion.tmall.config;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.phion.tmall.util.FileUtils;
+
+@Configuration
+public class ResourceConfigAdapter extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 获取文件的真实路径
+		String path = FileUtils.getResourcesPath()+"static\\img\\";
+		String os = System.getProperty("os.name");
+		if (os.toLowerCase().startsWith("win")) {
+			System.out.println("file:"+path);
+			registry.addResourceHandler("/img/**").addResourceLocations("file:" + path);
+		} else {// linux和mac系统 可以根据逻辑再做处理
+			registry.addResourceHandler("/img/**").addResourceLocations("file:" + path);
+		}
+		super.addResourceHandlers(registry);
+	}
+	
+	
+	
+}
